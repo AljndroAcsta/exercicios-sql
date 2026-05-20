0774537faec191c271a576ed9a3229e554509b70
Alejandro Noguera <alejandronoguera465@gmail.com>
2026-05-20 16:34:22 -0400

    adicionando exercicios 1 sobre ddl e dml

---
+++ ddl_e_dml/01_banco_faculdade.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE faculdade_db;
+use faculdade_db;
+
+CREATE TABLE alunos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    data_nascimento DATE NOT NULL,
+    curso VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE disciplinas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    codigo VARCHAR(10) NOT NULL UNIQUE,
+    carga_horaria INT NOT NULL
+);
+
+INSERT INTO alunos (nome, email, data_nascimento, curso) VALUES
+('João Silva', 'joao.silva@email.com', '2004-01-01', 'Engenharia de Software'),
+('Huan Oliveira', 'huan.oliveira@email.com', '2006-05-15', 'Ciência da Computação'),
+('Franco Tales', 'franco.tales@email.com', '2005-12-10', 'Administração'),
+('Victor Hugo', 'victor.hugo@email.com', '2001-03-20', 'Automação Industrial');
+
+INSERT INTO disciplinas (nome, codigo, carga_horaria) VALUES
+('Engenharia de Software', 'AED101', 60),
+('Ciência da Computação', 'BD102', 45),
+('Administração', 'RC103', 50),
+('Automação Industrial', 'SO104', 55);
+
+SELECT * FROM alunos;
+SELECT nome, curso FROM alunos;
+SELECT * FROM disciplinas WHERE carga_horaria > 60;
+
+UPDATE alunos 
+SET curso = 'Engenharia de Software
+' 
+WHERE nome = 'Franco Tales';
+
+UPDATE disciplinas
+SET carga_horaria = 65 
+WHERE nome = 'Engenharia de Software';
+
+DELETE FROM alunos 
+WHERE nome = 'Victor Hugo';
---
+++ ddl_e_dml/exerc_01.md
@@ -0,0 +1,35 @@
+Parte 1 – Criação da Estrutura (DDL)
+Crie o banco de dados chamado faculdade_db.
+
+Crie a tabela Aluno com os seguintes campos:
+id_aluno (inteiro, chave primária)
+nome (varchar 100)
+email (varchar 100)
+curso (varchar 50)
+
+Crie a tabela Disciplina com os campos:
+id_disciplina (inteiro, chave primária)
+nome_disciplina (varchar 100)
+carga_horaria (inteiro)
+
+Parte 2 – Inserção de Dados (DML).
+Insira 3 alunos na tabela Aluno.
+
+Insira 3 disciplinas na tabela Disciplina.
+
+Parte 3 – Consulta de Dados (SELECT).
+
+Liste todos os alunos cadastrados.
+
+Liste apenas o nome e o curso dos alunos.
+
+Liste as disciplinas cuja carga horária seja maior que 60 horas.
+
+
+Parte 4 – Atualização de Dados (UPDATE).
+Atualize o curso de um aluno para "Ciência da Computação".
+
+Altere a carga horária de uma disciplina específica.
+
+Parte 5 – Remoção de Dados (DELETE)
+Remova um aluno específico da tabela.
